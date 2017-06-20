f3d34c2b1ae6808d1a4779e742775a87d85fb0de
Haji Saleem <haji.saleem@mail.mcgill.ca>
2017-06-20 11:57:24 -0400

    running API

--- src/runAPI.py
+++ src/runAPI.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 '''
 Running the Perspective API
 The function run_Perspective takes as input a string and returns the tocxicity score
@@ -16,7 +17,7 @@ from datetime import datetime
 import time
 
 #DATA SETTINGS
-data_path = '../data/classifier/'
+data_path = '../data/raw/'
 file_name = sys.argv[1]
 file_path = os.path.join(data_path, file_name)
 
@@ -39,27 +40,35 @@ def run_Perspective(comment):
 scores = []
 
 counter = 0
+total_counter = 0
 a = datetime.now()
 for line in all_lines:
     jobj = json.loads(line)
     text = jobj['body']
     text = ' '.join(text.split())
-    try:
-        scores.append(run_Perspective(text))
-    except Exception, err:
-        scores.append('ERROR: '+str(err)[1:-1].split('returned ')[-1][1:-1])
-    counter += 1
-    if counter == 1000:
-        print 1000
-        b = datetime.now()
-        print b-a
-        tts = 100 - (b-a).seconds
-        if tts > 0:
-            print 'Sleeping', tts
-            time.sleep(tts)
-        counter = 0
-        a = datetime.now()
-        with open('API_'+file_name, 'a') as fout:
-            for line in scores:
-                fout.write(line+'\n')
-        scores = []
+    if text != '[deleted]':
+        try:
+            scores.append(run_Perspective(text))
+        except Exception, err:
+            scores.append('ERROR: '+str(err)[1:-1].split('returned ')[-1][1:-1])
+        counter += 1
+        total_counter += 1
+        if counter == 1000:
+            print 1000
+            b = datetime.now()
+            print b-a
+            tts = 100 - (b-a).seconds
+            if tts > 0:
+                print 'Sleeping', tts+5
+                time.sleep(tts+5)
+            counter = 0
+            a = datetime.now()
+            with open('API_'+file_name, 'a') as fout:
+                for line in scores:
+                    fout.write(line+'\n')
+            scores = []
+    if total_counter == 50000:
+        break
+with open('API_'+file_name, 'a') as fout:
+    for line in scores:
+        fout.write(line+'\n')
---
+++ src/runall.py
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sleep 100
+python runAPI.py 2xcrom.txt
+sleep 100
+python runAPI.py racism.txt
+sleep 100
+python runAPI.py loseit.txt
+sleep 100
+python runAPI.py random.txt
